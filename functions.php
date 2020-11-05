d705d1769b62f976079bb490bf83270226f7cdad
Tim Wu <tim@longevitygraphics.com>
2020-11-05 12:01:49 -0800

    dequeue recatpcha

--- functions.php
+++ functions.php
@@ -208,3 +208,17 @@ function mobile_sidebar(){
 	);
 	register_sidebar( $args );
 }
+
+// dequeue recaptcha scripts
+add_action('wp_print_scripts', function () {
+	wp_dequeue_script( 'google-recaptcha' );
+	wp_dequeue_script( 'wpcf7-recaptcha' );
+});
+
+// enqueue recaptcha scripts
+add_action('wp_print_scripts', function () {
+	if(is_page('contact')){
+		wp_enqueue_script( 'google-recaptcha' );
+		wp_enqueue_script( 'wpcf7-recaptcha' );
+	}
+});
